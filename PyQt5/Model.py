d7ce0124f10ba0ecb9fd1c53652845d2ea2603e7
2573943723 <67378023+2573943723@users.noreply.github.com>
2023-06-01 21:27:20 +0800

    20230601
    加入codesPlus的model

--- PyQt5/Model.py
+++ PyQt5/Model.py
@@ -4,12 +4,14 @@
 # @Author  : CaoQixuan
 # @File    : Model.py
 # @Description :
-import os
+"""
+日志
+20230529 修改了网络 ExtractFeature forward return squeeze-> squeeze(1)
+                  TextFeature_Bert forward squeeze-> squeeze(1)
+"""
 import pickle
-import random
 
 import numpy
-import numpy as np
 import torch
 import torchvision.transforms as standard_transforms
 from PIL import Image
@@ -17,25 +19,14 @@ from torch import nn
 from torchvision import transforms
 from transformers import AutoTokenizer
 
-from codes.Function import textEmbeddingPath, classEmbeddingPath, modelWightsDir, Load_ResNet50, try_gpu, textPrefix
+import codes
+import codesPlus
+from codes.Function import textEmbeddingPath, classEmbeddingPath, modelWightsDir, try_gpu, Load_ResNet50
 from codes.ImageRegionNet import ImageRegionNet
-from codes.NNManager import Net
-
-
-def set_seed(seed=1):  # seed的数值可以随意设置，本人不清楚有没有推荐数值
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    # cuDNN在使用deterministic模式时（下面两行），可能会造成性能下降（取决于model）
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
 
 class Model(nn.Module):
-    def __init__(self, modelWeightPath, device="cpu", textDir=None):
+    def __init__(self, modelWeightPath, device="gpu", textDir=None):
         super(Model, self).__init__()
         self.nHidden = 256  # 隐藏层 - Bi-LSTM
         self.seqLen = 80  # 步长 - Bi-LSTM
@@ -48,14 +39,13 @@ class Model(nn.Module):
         self.imageResize = transforms.Resize([480, 480])
 
         self.extractImageFeature = ImageRegionNet(net=Load_ResNet50())
-        self.net = self.net = Net(self.nHidden, self.seqLen, dropout=self.dropout,
-                                  classEmbeddingPath=classEmbeddingPath,
-                                  textEmbeddingPath=textEmbeddingPath, device=device, numLayers=self.numLayers)
+        self.net = self.net = codes.NNManager.Net(self.nHidden, self.seqLen, dropout=self.dropout,
+                                                  classEmbeddingPath=classEmbeddingPath,
+                                                  textEmbeddingPath=textEmbeddingPath, device=device,
+                                                  numLayers=self.numLayers)
         if device == "gpu":
             self.net.to(try_gpu())
-            self.extractImageFeature.to(try_gpu())
         self.net.load_state_dict(torch.load(modelWeightPath))
-        set_seed()
         self.net.eval()
         self.extractImageFeature.eval()
 
@@ -77,8 +67,6 @@ class Model(nn.Module):
         return numpy.pad(func(strs), (0, sqLen - len(strs)))
 
     def forward(self, text, imageFilePath):
-        # torch.Size([64, 80]) torch.Size([64, 196, 2048]) torch.Size([64, 5])
-        # torch.Size([64, 1, 80]) torch.Size([64, 1, 80]) torch.Size([64, 1, 80])
         with torch.no_grad():
             reText = torch.tensor(self.processText(self.seqLen, text), dtype=torch.int32).unsqueeze(0)
             reWord = torch.tensor([0, 0, 0, 0, 0]).unsqueeze(0)  # 类别默认为零
@@ -97,9 +85,52 @@ class Model(nn.Module):
             )).to("cpu")
 
 
+class ModelPlus(nn.Module):
+    def __init__(self, modelWeightPath, device="gpu", textDir=None):
+        super(ModelPlus, self).__init__()
+        self.nHidden = 256  # 隐藏层 - Bi-LSTM
+        self.seqLen = 80  # 步长 - Bi-LSTM
+        self.numLayers = 2  # 隐藏层层数
+        self.batchSize = 64  # 批量
+        self.maxClipping = 5  # 梯度裁剪
+        self.normType = 2  # 梯度的范式
+        self.dropout = 0  # DropOut层的概率 留取80%
+        self.net = self.net = codesPlus.NNManager.Net(self.nHidden, self.seqLen, dropout=self.dropout,
+                                                      classEmbeddingPath=classEmbeddingPath,
+                                                      textEmbeddingPath=textEmbeddingPath, device=device,
+                                                      numLayers=self.numLayers)
+        if device == "gpu":
+            self.net.to(try_gpu())
+        self.net.load_state_dict(torch.load(modelWeightPath))
 
-"""
-日志
-20230529 修改了网络 ExtractFeature forward return squeeze-> squeeze(1)
-                  TextFeature_Bert forward squeeze-> squeeze(1)
-"""
+        self.tokenizer = AutoTokenizer.from_pretrained(modelWightsDir + "bert-base-cased")
+        with open(textDir + "textVocab.py3", 'rb') as f:
+            self.word2id = pickle.load(f)  # 词表
+
+    def processText(self, sqLen, source):
+        """
+        :param sqLen:文本长度
+        :param source:字符串
+        :return:对应词表的对应SqLen长度
+        """
+        strs = source.split(" ")
+        if len(strs) > sqLen:
+            strs = strs[:sqLen]
+        strs = numpy.array(strs)
+        func = numpy.vectorize(lambda x: self.word2id[x] if x in self.word2id else self.word2id['<unk>'])
+        return numpy.pad(func(strs), (0, sqLen - len(strs)))
+
+    def forward(self, text, imageFilePath):
+        with torch.no_grad():
+            reText = torch.tensor(self.processText(self.seqLen, text), dtype=torch.int32).unsqueeze(0)
+            reWord = torch.tensor([0, 0, 0, 0, 0]).unsqueeze(0)  # 类别默认为零
+            image = self.imageToTensor(Image.open(imageFilePath).convert("RGB")).unsqueeze(0)
+            image = self.imageResize(image)
+            encodedInput = self.tokenizer(text, return_tensors='pt', padding="max_length", max_length=self.seqLen,
+                                          truncation=True)  # 模型编码
+            input_ids, token_type_ids, attention_mask = encodedInput["input_ids"].unsqueeze(0), encodedInput[
+                "token_type_ids"].unsqueeze(0), encodedInput["attention_mask"].unsqueeze(0)
+            return self.net((
+                reText, image, reWord,
+                (input_ids, token_type_ids, attention_mask)
+            )).to("cpu")
